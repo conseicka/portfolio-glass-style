434cc730f0f8d8cae59ac439d64fd6b570cc89cd
conseicka <>
2026-06-13 16:58:26 -0600

    add 404 page

--- second_style/app.py
+++ second_style/app.py
@@ -11,10 +11,12 @@ def home():
     return render_template('index.html', projects=projects)
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-    #config flask environment variable
-    # export FLASK_APP=app.py
-    # export FLASK_ENV=development
-    # flask run
+
     
